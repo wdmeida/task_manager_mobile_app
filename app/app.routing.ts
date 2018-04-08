c9b56fe6e3f4d5331fefc478f576e3d5561cbcb8
Wagner Almeida <wdmeida@gmail.com>
2018-04-08 11:46:22 -0300

    Config hooks

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,22 +1,11 @@
 import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 
-<<<<<<< HEAD
 const routes: Routes = [];
-=======
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
-const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent },
-];
->>>>>>> bcc72f4874eb682d570b9651bc6501f8c31efbc7
 
 @NgModule({
+    exports: [NativeScriptRouterModule],
     imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule { }
