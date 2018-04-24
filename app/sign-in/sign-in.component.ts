973c1fb8d4a70314b22af3b1dea6e03ac95c280b
Wagner Almeida <wdmeida@gmail.com>
2018-04-23 21:22:17 -0300

    Improving SignInComponent interface

--- app/sign-in/sign-in.component.ts
+++ app/sign-in/sign-in.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   moduleId: module.id,
   selector: "sign-in",
+  styleUrls: ["./sign-in.component.css"],
   templateUrl: "./sign-in.component.html",
 })
 
