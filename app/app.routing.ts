d3ab3ea5fb817b2075180e66f7f0734c419722a4
Wagner Almeida <wdmeida@gmail.com>
2018-04-25 20:07:41 -0300

    Setting up the AuthGuard

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,14 +2,17 @@ import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
-//  import pages
+// import pages
 import { HomeComponent } from "./home/home.component";
 import { SignInComponent } from "./sign-in/sign-in.component";
 import { SignUpComponent } from "./sign-up/sign-up.component";
 
+// import guard
+import { AuthGuard } from "./guards/auth.guard";
+
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
+  { path: "home", component: HomeComponent, canActivate: [ AuthGuard ] },
   { path: "sign-in", component: SignInComponent },
   { path: "sign-up", component: SignUpComponent },
 ];
